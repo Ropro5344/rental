67afa1755799112d71518c12d727a6b2b55b7330
Rohan Sharma <9028370@student.zadkine.nl>
2026-04-09 13:11:43 +0200

    'huur nu een auto' button klikbaar gemaakt

--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,7 @@
             <div class="advertorial">
                 <h2>Hét platform om een auto te huren</h2>
                 <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
-                <a href="#" class="button-primary">Huur nu een auto</a>
+                <a href="/ons-aanbod" class="button-primary">Huur nu een auto</a>
                 <img src="assets/images/car-rent-header-image-1.png" alt="">
                 <img src="assets/images/header-circle-background.svg" alt="" class="background-header-element">
             </div>
